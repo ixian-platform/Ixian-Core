1326ccb14efdcb5c7c8c3b9fbaa65d1cc87528fd
IxiAngel <33629337+IxiAngel@users.noreply.github.com>
2025-12-08 06:57:56 +0100

    xcore-0.9.7g

--- Meta/CoreConfig.cs
+++ Meta/CoreConfig.cs
@@ -20,7 +20,7 @@ namespace IXICore
         /// <summary>
         /// Current version of IxiCore.
         /// </summary>
-        public static readonly string version = "xcore-0.9.7f";
+        public static readonly string version = "xcore-0.9.7g";
 
         /// <summary>
         /// Current version of the Ixian network protocol.
